86a2c8b896efe344dd379c2d3b1deeb7a5f352fa
Nikhil Seetharaman <nikhilseetharaman@Nikhils-MBP.hotspot>
2017-08-08 23:52:34 +1000

    refactor

--- breakrepeatingkeyxor2.py
+++ breakrepeatingkeyxor2.py
@@ -5,7 +5,7 @@ import binascii
 '''
 def calc_str_score(s):
 	# Count total letter in input string
-	
+	s = s.lower()
 	letters = collections.Counter(s)
 	most_common = letters.most_common()
 	mc_array = []
@@ -14,13 +14,9 @@ def calc_str_score(s):
 	        k = k.lower()
 	        mc_array.append(k)
 	#print mc_array
-
 	topfivestring = str(mc_array[0]+mc_array[1]+mc_array[2]+mc_array[3]+mc_array[4]+mc_array[5]+mc_array[6]+mc_array[7]+mc_array[8]+mc_array[9]+mc_array[10])
-
 	#print topfivestring
-
 	score = 0
-
 	if 'e' in topfivestring:
 	    score += 1
 	if 'a' in topfivestring:
@@ -33,11 +29,10 @@ def calc_str_score(s):
 	    score += 1
 	if 'i' in topfivestring:
 	    score += 1
-
 	return score
 '''
 def calc_str_score(s):
-	
+	s = s.lower()
 	letters = collections.Counter(s)
 	most_common = letters.most_common()
 	# Remove spaces from char freq calculations
@@ -47,8 +42,8 @@ def calc_str_score(s):
 	    if k != ' ':
 	        k = k.lower()
 	        mc_array.append(k)
-		
-	topfivestring = str(mc_array[0]+mc_array[1]+mc_array[2]+mc_array[3]+mc_array[4]+mc_array[5]+mc_array[6]+mc_array[7])
+		#print mc_array
+	topfivestring = str(mc_array[0]+mc_array[1]+mc_array[2]+mc_array[3]+mc_array[4]+mc_array[5]+mc_array[6]+mc_array[7]+mc_array[8]+mc_array[9]+mc_array[10])
 
 	score = 0
 
@@ -66,22 +61,20 @@ def calc_str_score(s):
 	    score += 1
 
 	etaoinpass = False
-	if score >= 5:
+	if score >= 4:
 		etaoinpass = True
 
 	if etaoinpass == True:
 		if topfivestring[0] == 'e':
-			score += 4
-		if topfivestring[0] == 't' or topfivestring[1] == 't' or topfivestring[2] == 't':
-			score += 3
-		if topfivestring[0] == 'a' or topfivestring[1] == 'a' or topfivestring[2] == 'a':
-			score += 1
-		if topfivestring[0] == 'o' or topfivestring[1] == 'o' or topfivestring[2] == 'o':
+			score += 2
+		if topfivestring[0] == 't' or topfivestring[1] == 't':
 			score += 2
 
 	return score
 
 def xor(s1, s2):
+	s1 = s1.lower()
+	s2 = s2.lower()
 	return "".join(chr(ord(x) ^ ord(y)) for x, y in zip(s1, s2))
 
 def expandkey(key, ct):
@@ -92,7 +85,7 @@ def expandkey(key, ct):
 def bruteforce(s2):
 	#Preprocess ciphertext
 	s2 = s2.rstrip()
-	#s2 = s2.lower()
+	s2 = s2.lower()
 	#s2 = s2.decode("hex")
 
 	
@@ -139,8 +132,8 @@ def hammingDist (s1, s2):
 	return sum(c1 != c2 for c1,c2 in zip(s1,s2))
 
 def decryptMultiByteXOR(ct, k):
-	exp_k = expandkey(k, ct)
-	pt = xor(exp_k, ct)
+	exp_k = expandkey(k, ct.lower())
+	pt = xor(exp_k, ct.lower())
 	return pt
 
 def chunkstring(string, length):
@@ -151,7 +144,7 @@ def findKeySize():
 	best_KEYSIZE = 0
 	KEYSIZE = 1
 	while KEYSIZE <= 40:
-		chunks = list(chunkstring(ct, KEYSIZE*2))
+		chunks = list(chunkstring(ct.lower(), KEYSIZE*2))
 		numchunks = len(chunks)
 		hamsum = 0
 		mean_nhd = 0
@@ -168,15 +161,21 @@ def findKeySize():
 		KEYSIZE += 1
 	return best_KEYSIZE
 
+'''
+Test
+s1 = "this is a test"
+s2 = "wokka wokka!!!"
+print hammingDist(s1, s2) #should be 37
+'''
 
 # Load file, strip newlines, concat strings, b64 decode
-
 with open('6.txt') as f:
 	b64str = "".join(line.strip() for line in f)
-	ct = base64.b64decode(b64str)
+	ct = base64.b64decode(b64str).lower()
 
 # Grab KEYSIZE*2 characters from ct, find hamming distance between them
 # Divide by KEYSIZE to get normalized_hamming_distance (nhd), keep track of smallest and assoc keysize
+
 
 KEYSIZE = findKeySize()
 
@@ -210,8 +209,3 @@ k = ''.join(x for x in key)
 print k
 
 #print decryptMultiByteXOR(ct, k)
-
-
-
-
-
